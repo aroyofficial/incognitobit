ab137810e37050528872507e4a16cf82508ad0a7
aroy02072000 <aroy02072000@gmail.com>
2022-04-18 00:15:09 +0530

    Modified stego-functionality refs i0002

--- incognitobit/Steganography.py
+++ incognitobit/Steganography.py
@@ -24,7 +24,7 @@ class ImageNotCapableError(Exception, Decryption):
 # Using the algorithm discussed in the README.md
 class Steganography(Encryption):
 
-    # initialization of encryption object state
+    # Backend initialization for encryption
     def __init__(self, path = None, msg = None):
         if path is not None and msg is not None:
             try:
@@ -62,10 +62,15 @@ class Steganography(Encryption):
                 self.__encrypted_msg = self.__encryption_object._encryptMessage(list(self.__token))
             except:
                 pass
+        
+        # Backend initialization for decryption
         if path is not None and msg is None:
             try:
+                # Open stego image from the given path
                 self.__stego_image = Image.open(path, 'r')
+                # Extract height and width of image
                 self.__stego_image_width, self.__stego_image_height = self.__stego_image.size
+                # Extract the image array from the image object
                 self.__stego_image_arr = np.array(list(self.__stego_image.getdata()))
             except:
                 pass
@@ -93,17 +98,26 @@ class Steganography(Encryption):
 
     # hide encryption packet in the last row of cover image array
     def __hideEncryptionPacket(self):
+        # Get how many bits are required to embed stego key
         self.__bits_required_for_stego_key = math.ceil(math.log(self.__cover_image_width, 2))
+        # Get how many bits are required to embed private key
         self.__bits_required_for_private_key = 4
+        # Get how many bits are required to embed message length
         self.__bits_required_for_message_length = self.__cover_image_width - (self.__bits_required_for_stego_key + self.__bits_required_for_private_key)
+        # Get encryption packet (contains info for decryption)
         self.__getEncryptionPacket()
         index = 0
         i = -1
         inner_loop = False
+        # Iterate until the encryption packet embed
         while True:
             second_for_loop = False
+
+            # Iterate through each pixel
             for j in range(3):
                 temp = list(map(int, list(format(self.__cover_image_arr[i][j], "08b"))))
+                
+                # Red channel
                 if j == 0:
                     for k in range(3):
                         if index == self.__cover_image_width:
@@ -111,6 +125,8 @@ class Steganography(Encryption):
                             break
                         temp[-(k + 1)] = self.__encryption_info[index]
                         index += 1
+                
+                # Green channel
                 elif j == 1:
                     if index == self.__cover_image_width:
                         inner_loop = True
@@ -123,6 +139,8 @@ class Steganography(Encryption):
                             break
                         temp[-(k + 1)] = self.__encryption_info[index]
                         index += 1
+                
+                # Blue channel
                 else:
                     for k in range(2):
                         if index == self.__cover_image_width:
@@ -130,44 +148,59 @@ class Steganography(Encryption):
                             break
                         temp[-(k + 3)] = self.__encryption_info[index]
                         index += 1
+
                 temp = list(map(str, temp))
                 self.__cover_image_arr[i][j] = int("".join(temp), 2)
                 if inner_loop:
                     second_for_loop = True
                     break
+
             if second_for_loop:
                 break
             i -= 1
 
-    # hide plain text message within the cover image
+    # Hide plain text message within the cover image
     def __hideEncryptedMessage(self):
+        # Embed message bits
         for i in range(self.__stego_key, self.__stego_key + (self.__required_bits // 8)):
             index = 0
+
+            # Iterate through each pixel
             for j in range(3):
                 temp = list(map(int, list(format(self.__cover_image_arr[i][j], '08b'))))
+                
+                # Red channel
                 if j == 0:
                     for k in range(3):
                         temp[-(k + 1)] = int(self.__encrypted_msg[i - self.__stego_key][index: index + 1])
                         index += 1
+                
+                # Green channel
                 elif j == 1:
                     temp[-4] = int(self.__encrypted_msg[i - self.__stego_key][index: index + 1])
                     index += 1
                     for k in range(2):
                         temp[-(k + 1)] = int(self.__encrypted_msg[i - self.__stego_key][index: index + 1])
                         index += 1
+                
+                # Blue channel
                 else:
                     for k in range(2):
                         temp[-(k + 3)] = int(self.__encrypted_msg[i - self.__stego_key][index: index + 1])
                         index += 1
+
                 temp = list(map(str, temp))
                 self.__cover_image_arr[i][j] = int("".join(temp), 2)
 
+    # Generate stego image from cover image
     def _generateStegoImage(self):
         try:
-            # raise ImageNotCapableError("Image is not capable for hiding the message")
             self.__status = None
+
+            # If image is not capable for hiding the message
             if self.__cover_image_net_pixels - (self.__cover_image_width + self.__stego_key) < self.__required_bits // 8:
                 raise ImageNotCapableError("Image is not capable for hiding the message")
+
             self.__hideEncryptionPacket()
             self.__hideEncryptedMessage()
             self.__cover_image_arr  = self.__cover_image_arr.reshape(self.__cover_image_height, self.__cover_image_width, self.__cover_image_channel)
@@ -180,56 +213,76 @@ class Steganography(Encryption):
         finally:
             return self.__status
 
+    # Save stego image in the local machine
     def _saveStegoImage(self, filepath):
         self._stego_image.save(Path(filepath))
 
+    # Extract authorization token from the stego image
     def __getToken(self):
         self.__received_token = []
+
         for i in range(self.__stego_key, self.__stego_key + 16):
             binstring = ''
+            
             for j in range(3):
                 temp = list(format(self.__stego_image_arr[i][j], '08b'))
+                
                 if j == 0:
                     for k in range(3):
                         binstring += temp[-(k + 1)]
+                
                 elif j == 1:
                     binstring += temp[-4]
                     for k in range(2):
                         binstring += temp[-(k + 1)]
+                
                 else:
                     for k in range(2):
                         binstring += temp[-(k + 3)]
+            
             self.__received_token.append(binstring)
 
+    # Retrieve encrypted text from the stego image
     def __retrieveEncryptedText(self):
         self.__encrypted_msg = []
+        
         for i in range(self.__stego_key + 16, self.__stego_key + (self.__msg_length_in_bits // 8)):
             binstring = ''
+            
             for j in range(3):
                 temp = list(format(self.__stego_image_arr[i][j], '08b'))
+                
                 if j == 0:
                     for k in range(3):
                         binstring += temp[-(k + 1)]
+                
                 elif j == 1:
                     binstring += temp[-4]
                     for k in range(2):
                         binstring += temp[-(k + 1)]
+                
                 else:
                     for k in range(2):
                         binstring += temp[-(k + 3)]
+            
             self.__encrypted_msg.append(binstring)
             progress = (i - self.__stego_key + 16 + 1) // ((self.__msg_length_in_bits // 8) / 100)
             self.__makeProgress(progress)
 
+    # Get encryption packet from the stego image
+    # From which stego key, private key, and message length will be retrieved
     def __getEncryptionInfo(self):
         self.__encryption_info = []
         index = 0
         i = -1
         inner_loop = False
+        
         while True:
             second_for_loop = False
+            
             for j in range(3):
                 temp = list(format(self.__stego_image_arr[i][j], "08b"))
+                
                 if j == 0:
                     for k in range(3):
                         if index == self.__stego_image_width:
@@ -237,18 +290,21 @@ class Steganography(Encryption):
                             break
                         self.__encryption_info.append(temp[-(k + 1)])
                         index += 1
+                
                 elif j == 1:
                     if index == self.__stego_image_width:
                         inner_loop = True
                         break
                     self.__encryption_info.append(temp[-4])
                     index += 1
+                    
                     for k in range(2):
                         if index == self.__stego_image_width:
                             inner_loop = True
                             break
                         self.__encryption_info.append(temp[-(k + 1)])
                         index += 1
+                
                 else:
                     for k in range(2):
                         if index == self.__stego_image_width:
@@ -256,21 +312,26 @@ class Steganography(Encryption):
                             break
                         self.__encryption_info.append(temp[-(k + 3)])
                         index += 1
+                
                 if inner_loop:
                     second_for_loop = True
                     break
+            
             if second_for_loop:
                 break
             i -= 1
+        
         self.__private_key = int("".join(self.__encryption_info[:4]), 2)
         temp = math.ceil(math.log(self.__stego_image_width, 2))
         self.__stego_key = int("".join(self.__encryption_info[4: temp + 4]), 2)
         self.__msg_length_in_bits = int("".join(self.__encryption_info[temp + 4:]), 2)
 
+    # Update progress bar while retrieving the message from the stego image
     def __makeProgress(self, value):
         self.__loading_bar['value'] = value
         self.__root.update_idletasks()
 
+    # Retrieve message from stego image
     def _retrieveMessage(self, masterwidget, loadingbar):
         try:
             self.__root = masterwidget
